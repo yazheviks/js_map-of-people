39c214311b25453c56bef76e18a5c5bb7c39802e
Yazheviks <vicsergeyeva@gmail.com>
2020-10-06 19:28:01 +0300

    solution

--- src/mapOfPeople.js
+++ src/mapOfPeople.js
@@ -40,7 +40,17 @@
  * @return {Map}
  */
 function mapOfPeople(people) {
-  // write code here
+  const map = new Map();
+
+  people.forEach((item, index) => {
+    if (map.has(item)) {
+      map.delete(item);
+    } else {
+      map.set(item, index);
+    }
+  });
+
+  return map;
 }
 
 module.exports = mapOfPeople;
